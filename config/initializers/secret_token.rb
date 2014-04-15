f1d66ae71fc02a88776af06f97dcecae124617fc
Gooi <samgooi4189@gmail.com>
2014-04-15 18:19:08 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsMerge::Application.config.secret_key_base = 'e147a9648512f2d2b4094696e16eac06a6fd01393708a2ae587ad78f914e25c55d30612f244627e7fbe7853a5484f6c315484f2f60297ec84fe5558c8ca3ca6a'
